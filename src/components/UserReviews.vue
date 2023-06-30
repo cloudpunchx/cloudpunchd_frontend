1be7e22cb5c4f77f39cb0cf205031164c35b31d5
cloudpunchx <siobhanbonardi@hotmail.com>
2023-06-29 18:22:52 -0600

    Formatted UserReviews.vue, added alternate if no reviews exist for user.

--- src/components/UserReviews.vue
+++ src/components/UserReviews.vue
@@ -1,47 +1,51 @@
 <template>
     <div class="container">
-        <v-container v-for="movie in movies" :key="movie.ID">
-            <v-row no-gutters>
-                <v-col cols="auto">
-                    <router-link
-                    :to="'/movie/' + movie.MovieName + '/' + movie.movieId"
-                    >
-                        <v-img
-                            :src="movie.Poster"
-                            :width="80"
-                            class="poster"
+        <div v-if="movies.length > 0">
+            <v-container v-for="movie in movies" :key="movie.ID">
+                <v-row no-gutters>
+                    <v-col cols="auto">
+                        <router-link
+                        :to="'/movie/' + movie.MovieName + '/' + movie.movieId"
                         >
-                            <template v-slot:placeholder>
-                                <v-row
-                                    class="fill-height ma-0"
-                                    align-content="center"
-                                    justify="center"
-                                >
-                                    <v-progress-circular
-                                    indeterminate
-                                    color="grey-lighten-5"
-                                    ></v-progress-circular>
-                                </v-row>
-                            </template>
-                        </v-img>
-                    </router-link>
-                </v-col>
-                <v-col class="textBox">
-                    <h1 class="title">{{movie.MovieName }} <span class="dateSpan">{{ movie.Release_Date }}</span></h1>
-                    <v-rating
-                        :value="parseFloat(movie.Rating)"
-                        half-increments
-                        color="red"
-                        size="15"
-                        dense
-                        readonly
-                        empty-icon
-                        />
-                    <p>Watched {{ movie.watchedOn }}</p>
-                    <p class="bodyText">{{ movie.Review }}</p>
-                </v-col>
-            </v-row>
-        </v-container>
+                            <v-img
+                                :src="movie.Poster"
+                                :width="80"
+                                class="poster"
+                            >
+                            </v-img>
+                        </router-link>
+                    </v-col>
+                    <v-col class="textBox">
+                        <h1 class="title">{{movie.MovieName }} <span class="dateSpan">{{ movie.Release_Date }}</span></h1>
+                        <v-rating
+                            :value="parseFloat(movie.Rating)"
+                            half-increments
+                            color="red"
+                            size="15"
+                            dense
+                            readonly
+                            empty-icon
+                            />
+                        <p>Watched {{ movie.watchedOn }}</p>
+                        <p class="bodyText">{{ movie.Review }}</p>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-divider class="divider" color="#adb5bd"></v-divider>
+                </v-row>
+            </v-container>
+        </div>
+
+        <!-- Show 2nd container if there is no data for user review -->
+        <div v-else>
+            <v-container>
+                <v-row>
+                    <v-col cols="auto">
+                        <h2 class="altText">Write your first review!</h2>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </div>
     </div>
 </template>
 
@@ -103,5 +107,8 @@ import cookies from 'vue-cookies';
 .dateSpan{
     color: #adb5bd;
     font-size: 11pt;
+}
+.altText{
+    font-family: lobster;
 }
 </style>
